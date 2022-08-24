7cb24a16a31d5602970819b1dd7b8ebc1bcc9386
Renato Rossi <renatrossi@gmail.com>
2022-08-24 14:49:57 +0200

    Initial commit

--- dist/resi-dali-eth-ascii-client.js
+++ dist/resi-dali-eth-ascii-client.js
@@ -45,47 +45,13 @@ module.exports = function (RED) {
     RED.nodes.registerType("resi-dali-eth-ascii-client", function (config) {
         RED.nodes.createNode(this, config);
         var node = this;
+        node.log('dali-client [input] 1');
         node.connection = RESIConnectionPool.getInstance().getConnection(config.name, node, config);
-        // const host = config.address ;
-        // const port = config.port ? config.port : 502 ;
-        // node.engine = new telnetEngineLib.Engine(host, port) ;
-        // var statusBroadcaster = new openpromiseLib.Cycle() ;
-        // node.engine.timeOut = config.timeOut ? config.timeOut : 1000 ;
-        // node.engine.clearOut = config.clearOut ? config.clearOut : 0 ;
-        // node.engine.inDelimiter = config.inDelimiter ? RegExp(config.inDelimiter) : /\r\n|\r|\n/ ;
-        // node.engine.outDelimiter = config.outDelimiter ? config.outDelimiter.replace(/\\n/, '\n').replace(/\\r/, '\r').replace(/\\l/, '\l') : "\n" ;
-        // node.engine.modeStrict = false ;
-        // node.engine.autoFlush = 100 ;
-        // node.engine.autoFlush = config.openTries ? config.openTries : 1 ;
-        // statusBroadcaster.repeat( <nodered.NodeStatus> { fill: "grey", shape: "ring", text: "idle" } ) ;
-        // const onConnecting = node.engine.onConnecting(() => {
-        //     statusBroadcaster.repeat( <nodered.NodeStatus> { fill: "yellow", text: "connecting" } ) ;
-        // })
-        // const onSuccess = node.engine.onConnectionSuccess(() => {
-        //     statusBroadcaster.repeat( <nodered.NodeStatus> { fill: "green", text: "OK" } ) ;
-        //     node.log( "Connected to " + host + ":" + port ) ;
-        // })
-        // const onError = node.engine.onConnectionError(() => {
-        //     node.error( 'Connection error during communication' ) ;
-        //     statusBroadcaster.repeat( <nodered.NodeStatus> { fill: "red", text: "error" } ) ;
-        // })
-        // const onEnd = node.engine.onConnectionEnd(() => {
-        //     statusBroadcaster.repeat( <nodered.NodeStatus> { fill: "grey", text: "closed" } ) ;
-        //     node.log( 'Requested an end to the  connection' ) ;
-        // })
-        // const onConnectionTimeOut = node.engine.onConnectionTimeOut(() => {
-        //     statusBroadcaster.repeat( <nodered.NodeStatus> { fill: "red", text: "timeout" } ) ;
-        //     node.error( 'Connection wait exceeded timeout' ) ;
-        // })
-        // const onResponseTimeOut = node.engine.onResponseTimeOut(() => {
-        //     statusBroadcaster.repeat( <nodered.NodeStatus> { fill: "red", text: "timeout" } ) ;
-        //     node.error( 'Response wait exceeded timeout value (request ${node.engine.timeOut})' ) ;
-        // })
-        // const onReceive = node.engine.onReceive(() => {
-        //     statusBroadcaster.repeat( <nodered.NodeStatus> { fill: "green", text: "OK" } ) ;
-        // })
+        node.log('dali-client [input] 2');
         node.getStatusBroadcaster = () => { return node.connection.getNodeStatusBroadcaster(); };
+        node.log('dali-client [input] 3');
         this.on("input", (msg, send, done) => __awaiter(this, void 0, void 0, function* () {
+            node.log('dali-client [input]');
             done();
         }));
         this.on("close", (msg) => __awaiter(this, void 0, void 0, function* () {
